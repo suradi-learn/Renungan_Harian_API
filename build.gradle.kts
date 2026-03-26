1894134d7e1052aacd3d240bc943856c72180aeb
suradi-learn <suradi.learn@gmail.com>
2026-03-26 09:09:27 +0700

    change build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,8 @@ group = "com.suradi"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
